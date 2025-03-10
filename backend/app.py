415bf4f480194287fc73a14ddaf82abef2e62745
ThiagoDonato <donatothiago01@gmail.com>
2025-03-10 02:10:17 -0700

    chore: imported defaultdict to use in counting function

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, send_from_directory
+from collections import defaultdict
 import os
 
 app = Flask(__name__, static_folder='../frontend', static_url_path='')
